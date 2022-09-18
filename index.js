44eb3fd036df69e45b83ba4ad2d9024e3a5cb4a8
Faav <faavemc@gmail.com>
2022-09-18 12:13:17 -0400

    format

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ import { getUnformattedChat } from "../ClientsideChat";
 import sleep from '../sleep';
 
 register("worldLoad", () => {
-	register("command", (cmdType) => {
-		if (typeof cmdType == 'undefined') cmdType = 'tell';
-		sleep(50, () => {
-			Client.setCurrentChatMessage(`/minecraft:${cmdType} `);
-			ChatLib.chat("&ePlease press TAB!");
-			Client.showTitle("&e&lPlease press TAB!", "", 1, 25, 1);
-			sleep(2000, () => {
-				var recentChat = getUnformattedChat(5); // last 5 messages
-				var hasVanished = recentChat.filter((message) => {
-					if (message.includes(Player.getName()) && message.includes(', ')) return message;
-				});
-				if (hasVanished.length !== 0) {
-					var hasVanishedList = hasVanished[0].split(', ');
-					var noVanishedList = TabList.getUnformattedNames();
-					var vanishedList = hasVanishedList.filter((name) => noVanishedList.includes(name) == false);
-					ChatLib.chat("&9Vanished Players (By Faav):\n&l" + vanishedList.join(', '));
-					ChatLib.chat("&ePlease press ESC!");
-					Client.showTitle("&e&lPlease press ESC!", "", 1, 30, 1);
-				} else {
-					ChatLib.chat("&eYou didn't press TAB in time!");
-					Client.showTitle("&e&lYou didn't press TAB in time!", "", 1, 25, 1);
-				}
-			});
-		});
-	})
-	.setTabCompletions(["tell", "me", "w", "msg"])
-	.setName("vanishchecker")
-	.setAliases(["vanishcheck", "vcheck"]);
+  register("command", (cmdType) => {
+      if (typeof cmdType == 'undefined') cmdType = 'tell';
+      sleep(50, () => {
+        Client.setCurrentChatMessage(`/minecraft:${cmdType} `);
+        ChatLib.chat("&ePlease press TAB!");
+        Client.showTitle("&e&lPlease press TAB!", "", 1, 25, 1);
+        sleep(2000, () => {
+          var recentChat = getUnformattedChat(5); // last 5 messages
+          var hasVanished = recentChat.filter((message) => {
+            if (message.includes(Player.getName()) && message.includes(', ')) return message;
+          });
+          if (hasVanished.length !== 0) {
+            var hasVanishedList = hasVanished[0].split(', ');
+            var noVanishedList = TabList.getUnformattedNames();
+            var vanishedList = hasVanishedList.filter((name) => noVanishedList.includes(name) == false);
+            ChatLib.chat("&9Vanished Players (By Faav):\n&l" + vanishedList.join(', '));
+            ChatLib.chat("&ePlease press ESC!");
+            Client.showTitle("&e&lPlease press ESC!", "", 1, 30, 1);
+          } else {
+            ChatLib.chat("&eYou didn't press TAB in time!");
+            Client.showTitle("&e&lYou didn't press TAB in time!", "", 1, 25, 1);
+          }
+        });
+      });
+    })
+    .setTabCompletions(["tell", "me", "w", "msg"])
+    .setName("vanishchecker")
+    .setAliases(["vanishcheck", "vcheck"]);
 });
